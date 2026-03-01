3de3f50ef94d28e58de4c9f6e26ffc63ac1085ce
GoldenLys <rmdu45@gmail.com>
2026-03-01 01:33:29 +0100

    v1.30 - maj ideas

--- mod.user.js
+++ mod.user.js
@@ -18,8 +18,7 @@
 
 /* TODO & Ideas List
  - (Maybe) add "Stats" tab with more stats
- - Add a + button to the helper to select which level to reach 
- - With the plus button, make it generate a formula to reach the desired level with the less amount of time 
+ - Make the plus button generate a formula to reach the desired level with the less amount of time 
 */
 
 const KX = unsafeWindow;
